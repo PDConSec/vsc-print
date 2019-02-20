8d734296d4c72da755db7004cef8390bb4fa9ea3
Peter Wone <Peter.Wone@prowlerproof.com.au>
2019-02-20 18:09:41 +1000

    Successfully calling highlight.js

--- src/extension.ts
+++ src/extension.ts
@@ -1,18 +1,20 @@
 import * as vscode from 'vscode';
 import * as hljs from "highlight.js";
+import { readFileSync, writeFileSync } from 'fs';
+import { tmpNameSync } from 'tmp';
+// import * as css from "../node_modules/highlight.js/styles/ascetic.css";
 
 export function activate(context: vscode.ExtensionContext) {
-	console.log('VSC-PRINT ACTIVATED');
-	let disposable = vscode.commands.registerCommand('extension.print', () => {
-		// TODO IMPLEMENTATION
-		let editor = vscode.window.activeTextEditor;
-		if (editor) {
-			let language = editor.document.languageId;
-			let text = editor.document.getText();
-			let html = hljs.highlightAuto(text);
-			console.log(html);
-	}
-		vscode.window.showInformationMessage('Hello VS Code!');
+	// to avoid temp file pollution it may be necessary to switch to a pull model
+	// with the handler below merely spawning either a browser with a URL 
+	// as printcode does it, or my webprint service (which can request a URL)
+	let disposable = vscode.commands.registerCommand('extension.print', (file) => {
+		let src = readFileSync(file.fsPath).toString();
+		var css = "";//require("../node_modules/highlight.js/styles/ascetic.css");
+		let html = `<html><head><style>{css}</style></head><body>{hljs.highlightAuto(src).value}</body></html>`;
+		let fname = tmpNameSync({ postfix: ".html" });
+		let f = writeFileSync(fname, html);
+		
 	});
 	context.subscriptions.push(disposable);
 }
