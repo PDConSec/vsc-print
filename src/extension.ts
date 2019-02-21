102e3fe2a38894688055b905d093831b0ffe8111
Peter Wone <Peter.Wone@prowlerproof.com.au>
2019-02-21 16:17:27 +1000

    Webserver, browser launch and incomplete numbering

--- src/extension.ts
+++ src/extension.ts
@@ -1,34 +1,111 @@
 import * as vscode from 'vscode';
 import * as hljs from "highlight.js";
-import { readFileSync, writeFileSync, readFile } from 'fs';
-import { tmpNameSync } from 'tmp';
-// import * as css from "../node_modules/highlight.js/styles/ascetic.css";
+import { readFileSync, createReadStream } from 'fs';
+import * as http from "http";
+import * as child_process from "child_process";
 
 export function activate(context: vscode.ExtensionContext) {
-	// to avoid temp file pollution it may be necessary to switch to a pull model
-	// with the handler below merely spawning either a browser with a URL 
-	// as printcode does it, or my webprint service (which can request a URL)
-	let disposable = vscode.commands.registerCommand('extension.print', (file) => {
-		readFile(file.fsPath, (err, data) => {
-			var src = data.toString();
-			let x = vscode.extensions.getExtension("pdconsec.print");
-			if (x) {
-				var stylePath = `${x.extensionPath}/node_modules/highlight.js/styles`;
-				readFile(`${stylePath}/default.css`, (err, data) => {
-					var defaultCss = data.toString();
-					readFile(`${stylePath}/ascetic.css`, (err, data) => {
-						var swatchCss = data.toString();
-						let html = `<html><head><style>${defaultCss}\r${swatchCss}</style></head><body><pre><code>${hljs.highlightAuto(src).value}</code></pre></body></html>`;
-						let fname = tmpNameSync({ postfix: ".html" });
-						let f = writeFileSync(fname, html);
-						console.log(fname);
-					});
-				});
-			}
-		});
-		// let html = `<html><head><link rel="stylesheet" type="text/css" href="K:\extension-1\vsc-print\node_modules\highlight.js\styles\default.css"><link rel="stylesheet" type="text/css" href="K:\extension-1\vsc-print\node_modules\highlight.js\styles\ascetic.css"></head><body><pre><code>${hljs.highlightAuto(src).value}</code></pre></body></html>`;
+	printConfig = vscode.workspace.getConfiguration("print", null);
+	let disposable = vscode.commands.registerCommand('extension.print', cmdArgs => {
+		commandArgs = cmdArgs;
+		startWebserver();
+		child_process.exec(`${printConfig.browserPath || browserMap[process.platform]} http://localhost:${printConfig.port}/`);
 	});
 	context.subscriptions.push(disposable);
+}
+
+var commandArgs: any;
+var printConfig: vscode.WorkspaceConfiguration;
+
+const browserMap: any = {
+	darwin: "open",
+	linux: "xdg-open",
+	win32: "start"
+};
+
+function getFileText(fname: string): string {
+	var text = readFileSync(fname).toString();
+	//strip BOM when present
+	return text.indexOf('\uFEFF') === 0 ? text.substring(1, text.length) : text;
+}
+
+function getSourceCode(): string {
+	try {
+		var lines = getFileText(commandArgs.fsPath).split("\n");
+		var text = "";
+		for (let i = 0; i < lines.length; i++) {
+			const line = lines[i];
+			text += `${i} ${line}`;
+		}
+		return text;
+		//return getFileText(commandArgs.fsPath);
+	} catch (error) {
+		if (vscode.window.activeTextEditor) {
+			return vscode.window.activeTextEditor.document.getText();
+		} else {
+			throw new Error("Cannot access the specified file and there is no active editor");
+		}
+	}
+}
+
+function getRenderedSourceCode(): string {
+	let x = vscode.extensions.getExtension("pdconsec.print");
+	if (!x) { throw new Error("Cannot resolve extension. Has the name changed?"); }
+	let stylePath = `${x.extensionPath}/node_modules/highlight.js/styles`;
+	let defaultCss = getFileText(`${stylePath}/default.css`);
+	let swatchCss = getFileText(`${stylePath}/kimbie.light.css`);
+	let renderedCode = hljs.highlightAuto(getSourceCode()).value;
+	return `<html><head><style>${defaultCss}\r${swatchCss}</style></head><body style="font-family: Consolas, monospace;font-size:${printConfig.fontSize};" onload="window.print();window.close();"><pre><code>${renderedCode}</code></pre></body></html>`;
+}
+
+var server: http.Server | undefined;
+var port: number = 5050;
+
+function startWebserver(): Promise<void> {
+	return new Promise((resolve, reject) => {
+		// clean up unexpected stragglers
+		if (server !== undefined && printConfig.port !== port) {
+			server.close(() => { });
+			server = undefined;
+		}
+		if (server === undefined) {
+			// prepare to service an http request
+			server = http.createServer((request, response) => {
+				if (request.url) {
+					let html = getRenderedSourceCode();
+					//response.setHeader("Content-Type", "text/html");
+					response.end(html);
+				}
+			});
+			// report exceptions
+			server.on("error", (err: any) => {
+				if (err) {
+					switch (err.code) {
+						case "EADDRINUSE":
+							vscode.window.showInformationMessage(`PORT ${printConfig.port} OCCUPIED. CHANGE WEBSERVER CONFIG.`);
+							break;
+						case "EACCES":
+							vscode.window.showInformationMessage("ACCESS DENIED ESTABLISHING WEBSERVER");
+							break;
+					}
+					if (server) {
+						server.close();
+						server = undefined;
+						port = 0;
+					}
+					reject();
+				}
+			});
+			// clean up after one request
+			server.on("request", (request: any, response: any) => {
+				response.on("finish", () => {
+					request.socket.destroy();
+				});
+			});
+			server.listen(printConfig.port);
+		}
+		resolve();
+	});
 }
 
 export function deactivate() { }
