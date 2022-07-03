dc6ac35b2d5cee24bdc2d05d46563ab84311bdee
Peter Wone <peter.wone@outlook.com>
2022-07-03 23:47:21 +1000

    Grooming

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -19,20 +19,18 @@ suite('Print Extension Test Suite', () => {
 	test(`Check platform browser launch command on ${process.platform}`, async () => {
 		const printConfig = vscode.workspace.getConfiguration("print", null);
 		await printConfig.update("alternateBrowser", false);
-		let cmd: string | undefined;
+		const cmd = await vscode.commands.executeCommand<string>("extension.test.browserLaunchCommand");
 		switch (process.platform) {
+			
 			case "win32":
-				cmd = await vscode.commands.executeCommand<string>("extension.test.browserLaunchCommand", true);
 				assert.strictEqual(cmd, "start");
 				break;
 
 			case "linux":
-				cmd = await vscode.commands.executeCommand<string>("extension.test.browserLaunchCommand", true);
 				assert.strictEqual(cmd, "xdg-open");
 				break;
 
 			case "darwin":
-				cmd = await vscode.commands.executeCommand<string>("extension.test.browserLaunchCommand", true);
 				assert.strictEqual(cmd, "open");
 				break;
 		}
@@ -45,7 +43,7 @@ suite('Print Extension Test Suite', () => {
 		switch (process.platform) {
 			case "win32":
 				await printConfig.update("browserPath", "c:\\Program Files\\Google\\Chrome\\Application\\chrome.exe");
-				cmd = await vscode.commands.executeCommand<string>("extension.test.browserLaunchCommand", true);
+				cmd = await vscode.commands.executeCommand<string>("extension.test.browserLaunchCommand");
 				assert.strictEqual(cmd, '"c:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"');
 				break;
 
@@ -66,22 +64,22 @@ suite('Print Extension Test Suite', () => {
 		flags?.add("suppress browser");
 		await vscode.window.showTextDocument(otd);
 		assert.ok(vscode.window.activeTextEditor);
-		const session = await vscode.commands.executeCommand<string>("extension.print") as PrintSession | undefined;
-		await session!.ready;
-		const url = session!.getUrl();
+		const session = (await vscode.commands.executeCommand<PrintSession>("extension.print"))!;
+		await session.ready;
+		const url = session.getUrl();
 		const response = await axios.get(url);
 		assert.equal(response.headers["content-type"], 'text/html; charset=utf-8');
 		assert.ok(response.data.includes("<title>sample.json</title>"));
 	});
 
 	test('Print folder', async () => {
-		const W = vscode.workspace.workspaceFolders;
-		let w = W![0].uri.fsPath;
-		const flags = await vscode.commands.executeCommand<Set<string>>("extension.test.flags");
-		flags?.add("suppress browser");
-		const session = await vscode.commands.executeCommand<string>("extension.printFolder", W![0].uri) as PrintSession | undefined;
-		await session!.ready;
-		const url = session!.getUrl();
+		const W = vscode.workspace.workspaceFolders!;
+		let w = W[0].uri.fsPath;
+		const flags = (await vscode.commands.executeCommand<Set<string>>("extension.test.flags"))!;
+		flags.add("suppress browser");
+		const session = (await vscode.commands.executeCommand<PrintSession>("extension.printFolder", W![0].uri))!;
+		await session.ready;
+		const url = session.getUrl();
 		const response = await axios.get(url);
 		assert.equal(response.headers["content-type"], 'text/html; charset=utf-8');
 		assert.ok(response.data.includes("<title>test-docs</title>"));
