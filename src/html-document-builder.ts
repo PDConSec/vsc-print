e7bd64ce81063df0983fb7d098414ca9185acd1b
Peter Wone <peter.wone@outlook.com>
2022-10-24 08:55:03 +1000

    remove unused function

--- src/html-document-builder.ts
+++ src/html-document-builder.ts
@@ -157,36 +157,7 @@ export class HtmlDocumentBuilder {
 			return pre + line + "</span>".repeat(classes.length);
 		}).join("\n");
 	}
-	Utf8ArrayToStr(array: Uint8Array): string {
-		var out, i, len, c;
-		var char2, char3;
-		out = "";
-		len = array.length;
-		i = 0;
-		while (i < len) {
-			c = array[i++];
-			switch (c >> 4) {
-				case 0: case 1: case 2: case 3: case 4: case 5: case 6: case 7:
-					// 0xxxxxxx
-					out += String.fromCharCode(c);
-					break;
-				case 12: case 13:
-					// 110x xxxx   10xx xxxx
-					char2 = array[i++];
-					out += String.fromCharCode(((c & 0x1F) << 6) | (char2 & 0x3F));
-					break;
-				case 14:
-					// 1110 xxxx  10xx xxxx  10xx xxxx
-					char2 = array[i++];
-					char3 = array[i++];
-					out += String.fromCharCode(((c & 0x0F) << 12) |
-						((char2 & 0x3F) << 6) |
-						((char3 & 0x3F) << 0));
-					break;
-			}
-		}
-		return out;
-	}
+	
 	async docsInFolder(): Promise<vscode.TextDocument[]> {
 		logger.debug(`Enumerating the files in ${this.filename}`);
 		const printConfig = vscode.workspace.getConfiguration("print", null);
