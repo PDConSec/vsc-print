0639e3e5dc278dab0af6329570a1800d388a9368
Peter Wone <peter.wone@outlook.com>
2022-10-25 21:06:28 +1000

    html-renderer-markdown

--- src/html-renderer-markdown.ts
+++ src/html-renderer-markdown.ts
@@ -1,24 +1,30 @@
 import * as vscode from 'vscode';
 import { logger } from './logger';
-import hljs = require('highlight.js');
+import { HtmlDocumentBuilder } from './html-document-builder';
 
-export function getBodyHtml(raw: string, languageId: string): string {
+export function getBodyHtml(raw: string): string {
 	let renderedCode = "";
+	try {
+		renderedCode = HtmlDocumentBuilder.MarkdownEngine.render(raw);
+		const v = renderedCode.lastIndexOf("</style>");
+		if (v != -1) {
+			renderedCode = renderedCode.substring(v + 8);
+		}
+	} catch {
+		logger.error("Markdown could not be rendered");
+		renderedCode = "<div>Could not render this file.</end>";
+	}
 	return renderedCode;
 }
 
 export function getCssUriStrings(): Array<string> {
+	const markdownConfig = vscode.workspace.getConfiguration("markdown");
 	return [
-		"vsc-print.resource/default.css",
-		"vsc-print.resource/line-numbers.css",
-		"vsc-print.resource/colour-scheme.css",
-		"vsc-print.resource/settings.css",
+		"vsc-print.resource/default-markdown.css",
+		...markdownConfig.styles
 	];
 }
 
-function getEmbeddedStyles() {
-	let editorConfig = vscode.workspace.getConfiguration("editor");
-	return `body{tab-size:${editorConfig.tabSize};}`;
+export function isEnabled(): boolean {
+	return vscode.workspace.getConfiguration("print").renderMarkdown;
 }
-
-
