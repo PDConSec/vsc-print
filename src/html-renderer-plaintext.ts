4d5bc8d5f49b71d17fcaf4986a15e18e2105c122
Peter Wone <peter.wone@outlook.com>
2023-05-27 18:57:58 +1000

    Escape HTML

--- src/html-renderer-plaintext.ts
+++ src/html-renderer-plaintext.ts
@@ -1,3 +1,5 @@
+import { escapeHtml } from "markdown-it/lib/common/utils";
+
 export function getBodyHtml(raw: string): string {
-	return `<pre>\n${raw}</pre>`;
+	return `<pre>\n${escapeHtml(raw)}</pre>`;
 }
