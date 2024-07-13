51a542d717ceb49f3a08b69370680d3e189c8ea2
Peter Wone <peter.wone@outlook.com>
2024-07-13 18:20:38 +1000

    add support for favicon

--- gather.js
+++ gather.js
@@ -5,6 +5,7 @@ const path = require('path');
   await fs.rm("dist", { recursive: true });
   await fs.mkdir("dist", { recursive: true });
   await fs.cp("node_modules/katex/dist/fonts", "dist/fonts", { recursive: true });
+  await fs.copyFile("assets/favicon.ico", "dist/favicon.ico");
   await fs.copyFile("node_modules/katex/dist/katex.css", "dist/katex.css");
   await fs.copyFile("node_modules/smiles-drawer/dist/smiles-drawer.min.js", "dist/smiles-drawer.min.js");
   await fs.copyFile("node_modules/smiles-drawer/dist/smiles-drawer.min.js.map", "dist/smiles-drawer.min.js.map");
